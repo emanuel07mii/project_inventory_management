5e28cbdd83ca286d95bda6eea41613099c1b9218
Emanuel Miranda <emanuelmiranda932@gmail.com>
2025-04-04 18:04:23 -0300

    Forms Django - Aula 09

---
+++ form_project/form_app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from .form import ContactForm
+# Create your views here.
+
+def home_view(request):
+    return render(request, 'form_app/home.html')
+
+# Define a view de contato para visualizar o form de contato
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.send_email()
+            return redirect('contact-success')
+        else:
+            form = ContactForm()
+        context = {'form':form}
+        return render(request, 'form_app/contact.html', context)
+    form = ContactForm()
+    context = {'form':form}
+    return render(request, 'form_app/contact.html', context)
+
+# Função para manipular a página de success
+def contact_success_view(request):
+    return render(request, 'form_app/contact_success.html')
